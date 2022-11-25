332e22a4da19214b7b3e485e29d18e684225d0be
MikeMG-PL <galinski.06@vp.pl>
2022-11-25 22:56:32 +0100

    Project setup

---
+++ Source/ThePowerOfFourLeaves/ThePowerOfFourLeaves.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ThePowerOfFourLeaves.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThePowerOfFourLeaves, "ThePowerOfFourLeaves" );
+ 
---
+++ Source/ThePowerOfFourLeaves/ThePowerOfFourLeavesGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThePowerOfFourLeavesGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AThePowerOfFourLeavesGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AThePowerOfFourLeavesGameMode();
+};
+
+
+
